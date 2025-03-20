696f60f877a32446f3c9fb9fb8713a63b69651b4
mtrigueros <moisestrigueros@hotmail.com>
2025-03-20 15:31:26 -0600

    remove unused function

--- src/firebase/hooks/useCurrentUser.ts
+++ src/firebase/hooks/useCurrentUser.ts
@@ -1,8 +1,7 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, database } from '@/firebase/database/firebase-config';
 import { ref, get } from 'firebase/database';
-import { getUsername, getSanitizedEmail } from '@/lib/utils';
 
 export interface User {
   email: string;
@@ -30,57 +29,3 @@ export function useCurrentUser() {
 
   return { user, loading, error };
 }
-
-export const useGetUsers = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const usersRef = ref(database, 'users');
-    get(usersRef)
-      .then((snapshot) => {
-        const usersData = snapshot.val();
-        if (usersData) {
-          const usersArray = Object.entries(usersData).map(([uid, user]) => {
-            const typedUser = user as Partial<User>;
-            return {
-              ...(typeof typedUser === 'object' && typedUser !== null
-                ? typedUser
-                : {}),
-              uid,
-              email: typedUser.email || '',
-              name: typedUser.name || '',
-              inviteCode: typedUser.inviteCode || '',
-              loggedInAt: typedUser.loggedInAt || '',
-            };
-          });
-          setUsers(usersArray);
-        }
-      })
-      .catch((err) => {
-        setError(err.message);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  const getNameByEmail = useCallback(
-    (email: string) => {
-      return (
-        users.find(
-          (user) => getSanitizedEmail(user.email) === getSanitizedEmail(email),
-        )?.name || getUsername(email)
-      );
-    },
-    [users],
-  );
-
-  return {
-    error,
-    getNameByEmail,
-    loading,
-    users,
-  };
-};
