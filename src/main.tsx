d4fec747b8ade5eecc3eba58218c8ca639dd39aa
Reymundo Tenorio <reymundotenorio@gmail.com>
2025-03-19 18:13:24 -0600

    refactor(main): removing unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,21 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-    <RouterProvider router={router} />
-);
+root.render(<RouterProvider router={router} />);
