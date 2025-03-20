d8884228c8396fc2b20d1f95b85fdd18db1e4bc5
Hens94 <henry_gadea@hotmail.com>
2025-03-19 23:31:37 -0600

    feat: Improve event stats table

--- src/components/GamePositionTable.tsx
+++ src/components/GamePositionTable.tsx
@@ -11,43 +11,52 @@ const TABLE_HEADERS = Object.freeze([
 ]);
 
 const GamePositionTable = () => {
-  const { scores } = useTopUsersForEvent('sprint');
-  const { user } = useCurrentUser();
+  const { scores, loading: isLoadingStats } = useTopUsersForEvent('sprint');
+  const { user, loading: isLoadingUser } = useCurrentUser();
+
+  const isLoading = isLoadingStats || isLoadingUser;
 
   return (
-    <div className="nes-container is-rounded flex h-fit flex-col items-center gap-4 bg-gray-200 p-8">
+    <div className="nes-container is-rounded flex h-fit w-full flex-col items-center gap-4 bg-gray-200 p-8">
       <h1 className="text-2xl font-bold">Event Stats</h1>
-      <Table>
-        <thead>
-          <tr>
-            {TABLE_HEADERS?.map(({ key, title }) => <th key={key}>{title}</th>)}
-          </tr>
-        </thead>
 
-        <tbody>
-          {scores?.map((score, index) => {
-            const email = score.email;
-            const sanitizedEmail = user?.email.replace(/\./g, '_');
-            const isCurrentUser = email === sanitizedEmail;
-            return (
-              <tr
-                key={email}
-                className={cn(
-                  'whitespace-nowrap text-black/50 transition-colors',
-                  {
-                    'bg-red-100 font-bold text-red-500/100': isCurrentUser,
-                  },
-                )}
-              >
-                <td>{index + 1}</td>
-                <td>{getUsername(email)}</td>
-                <td>{score.score.distanceTraveled} m</td>
-                <td>{getTime(score.score.finishTime)}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Table>
+          <thead>
+            <tr>
+              {TABLE_HEADERS?.map(({ key, title }) => (
+                <th key={key}>{title}</th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {scores?.map((score, index) => {
+              const email = score.email;
+              const sanitizedEmail = user?.email.replace(/\./g, '_');
+              const isCurrentUser = email === sanitizedEmail;
+              return (
+                <tr
+                  key={email}
+                  className={cn(
+                    'whitespace-nowrap text-black/50 transition-colors',
+                    {
+                      'bg-red-100 font-bold text-red-500/100': isCurrentUser,
+                    },
+                  )}
+                >
+                  <td>{index + 1}</td>
+                  <td>{getUsername(email)}</td>
+                  <td>{score.score.distanceTraveled} m</td>
+                  <td>{getTime(score.score.finishTime)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 };
