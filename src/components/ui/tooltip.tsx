cbf8ffea62d7977c0ecbe29b93685138b11bc3db
Reymundo Tenorio <reymundotenorio@gmail.com>
2025-03-20 11:22:40 -0600

    refactor(utils): creating util for total distance

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,24 +1,24 @@
-import { getUsername } from '@/lib/utils';
+import { getDistance } from '@/lib/utils';
 import { FC } from 'react';
 
 export interface TooltipProps {
   visible: boolean;
   x: number;
   y: number;
-  email: string;
-  distance: string;
+  name: string;
+  distance: number;
 }
 
-const Tooltip: FC<TooltipProps> = ({ distance, email, x, y }) => {
+const Tooltip: FC<TooltipProps> = ({ distance, name, x, y }) => {
   return (
     <div
       className="pointer-events-none absolute -translate-x-1/2 -translate-y-full transform whitespace-nowrap bg-black bg-opacity-70 px-2 py-1 text-xs text-white"
       style={{ left: x, top: y }}
     >
       <div>
-        <strong>{getUsername(email)}</strong>
+        <strong>{name}</strong>
       </div>
-      <div>{distance} m</div>
+      <div>{getDistance(distance)}</div>
     </div>
   );
 };
